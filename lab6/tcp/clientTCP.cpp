69eebe216d252466f1739ace12b64b425fef2ab6
Pavel Manakov <james.swift@yandex.ru>
2017-10-20 23:44:59 +0700

    tcp on pthread. stable version

--- lab6/tcp/clientTCP.cpp
+++ lab6/tcp/clientTCP.cpp
@@ -1,62 +1,59 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main (int argc, char **argv){
-	int port;
-	char message[1], filename[255], buf[1];
-    int sock; 
-	struct sockaddr_in addr;
-	char *hostinfo;
+#define SIZE 1024
+#define SERVER "127.0.0.1"
+#define PORT 55555
 
-	if(argc != 3){
-		printf("Не верное кол-во аргументов!\nДолжно быть 2 аргумента (Порт,ip-адрес -сервера)!\n");
-		exit(0);
-	}
+int main(int argc, char **argv){
+    int sockfd;
+    int n;
+    char sendline[SIZE],recvline[SIZE];
+    struct sockaddr_in servaddr;
+    bzero(sendline,SIZE);
+    bzero(recvline,SIZE);
+    if((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+        perror(NULL);
+        exit(1);
+    }
+    bzero(&servaddr, sizeof(servaddr));
 
-	port = atoi(argv[1]);
-	hostinfo = argv[2];
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	
-	addr.sin_family = AF_INET; 
-	addr.sin_port = htons(port); 
-	addr.sin_addr.s_addr = inet_addr(hostinfo);
-	
-	connect(sock, (struct sockaddr *)&addr, sizeof(addr));
-	
-	printf("Input filename: ");
-	scanf("%s", filename);
-	send(sock, filename, sizeof(filename), 0); 
-	FILE *f1 = fopen(filename, "rb");
-	int n;
-	while(1){
-		n = fread(message, sizeof(char), sizeof(message), f1);
-		if(n == 0){
-			send(sock, "OK", 1024, 0);
-			printf("Файл успешно передан\n");
-			break;
-		}
-		else{
-			printf("Отправка сообщения на сервер...\n");
-			send(sock, message, sizeof(message), 0); 
-			int bytes_read = 0;
-			printf("Ожидание сообщения\n");
-			bytes_read = recv(sock, buf, sizeof(buf), 0);
-			if(bytes_read < 0){
-				close(sock);
-				exit(0);
-			}
-			else 
-				printf("Получено %d bytes\tСообщение: \n%s\n", bytes_read, buf); 
-		}
-	}
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    if(inet_aton(SERVER, &servaddr.sin_addr) == 0){
+        printf("Некорректный АЙПИ\n");
+        close(sockfd);
+        exit(1);
+    }
+    if(connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0){
+        perror(NULL);
+        close(sockfd);
+        exit(1);
+    }
 
-	fclose(f1);
-	close(sock);
+    while(1){
+        printf("Сообщение: ");
+        fflush(stdin);
+        fgets(sendline, SIZE, stdin);
+        if( (n = write(sockfd, sendline, strlen(sendline)+1)) < 0){
+            perror("Can\'t write\n");
+            close(sockfd);
+            exit(1);
+        }
+        if ((n = read(sockfd,recvline, 999)) < 0){
+            perror("Can\'t read\n");
+            close(sockfd);
+            exit(1);
+        }
+        cout << recvline;
+    }
 
-	return 0;
+    close(sockfd);
 }
--- lab6/tcp/serverTCP.cpp
+++ lab6/tcp/serverTCP.cpp
@@ -1,83 +1,78 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/wait.h>
 #include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <errno.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <pthread.h>
 
-#define PORT 55555
-#define SIZE 16
+using namespace std;
+
+void *stream(void *args){
+	int newsockfd = *(int*)args;
+	int n;
+	char line[1024];
+	while((n = read(newsockfd, line, 1024)) > 0){
+        	string str = line;
+        	cout << "str: " << str << endl;
+            if((n = write(newsockfd, line, strlen(line)+1)) < 0){
+                perror(NULL);
+                close(newsockfd);
+                exit(1);
+            }
+        }
+    	if(n < 0){
+            perror(NULL);
+            close(newsockfd);
+            exit(1);
+        }
+        close(newsockfd);
+}
 
 int main(){
-    int sock, listener, status;       
-    struct sockaddr_in addr, client;
-    int bytes_read = 0;
-    socklen_t slen = sizeof(addr);
-	char buf[SIZE], buf_fname[SIZE];
-	char filename[255];
-	pid_t childPid, endID;
-    listener = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd, newsockfd;
+    socklen_t clilen;
+    int n;
+    char line[1024];
+    struct sockaddr_in servaddr, cliaddr;
     
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(55555);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    
-    bind(listener, (struct sockaddr *)&addr, slen);
-    
-    getsockname(listener, (struct sockaddr*)&addr, &slen);
-    listen(listener, 3);
-
-    printf("PORT: %d\n", htons(addr.sin_port));
-
-	printf("%s", filename);
-    
-    while(1){
-		sock = accept(listener, (struct sockaddr *)&client, (socklen_t*)&slen);
-		if(sock < 0){
-			perror("Прием входящих подключений");
-			exit(3);
-		}
-		
-		switch(childPid = fork()){
-			case -1:
-				perror("fork");
-				break;
-			case 0:{
-				recv(sock, buf_fname, SIZE, 0);
-				sprintf(filename, "%s:%d_%s", inet_ntoa(client.sin_addr), ntohs(client.sin_port), buf_fname);
-				FILE *f1 = fopen(filename, "wb");
-				close(listener);
-				
-				while(1){
-					printf("Ожидаем сообщение...\n");
-					bytes_read = recv(sock, buf, sizeof(buf), 0);
-					if(bytes_read <= 0){
-						close(sock);
-						exit(EXIT_SUCCESS);
-					}
-					if(strcmp("OK", buf) == 0){
-						printf("Файл успешно принят от клиента: %s:%d", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
-						send(sock, buf, bytes_read, 0);
-						close(sock);
-						fclose(f1);
-						exit(0);
-					}
-					
-					fwrite(buf, 1, sizeof(buf), f1);
-					printf("Получено %d bytes от клиента %s:%d\n", bytes_read, inet_ntoa(client.sin_addr), ntohs(client.sin_port));
-					printf("Отправляю принятое сообщение клиенту\n");
-					if(send(sock, buf, bytes_read, 0) < 0){
-						 close(sock);
-						 exit(EXIT_SUCCESS);
-					}
-					memset(buf, 0, SIZE);
-				}
-			}
-			break;
-		}	    
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        perror(NULL);
+        exit(1);
     }
-    return 0;
+
+    pthread_t pt;
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(55555);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if(bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0){
+        perror(NULL);
+        close(sockfd);
+        exit(1);
+    }
+    
+    if(listen(sockfd, 5) < 0){
+        perror(NULL);
+        close(sockfd);
+        exit(1);
+    }
+
+    while(1){
+        clilen = sizeof(cliaddr);
+        if((newsockfd = accept(sockfd, (struct sockaddr *) &cliaddr, &clilen)) < 0){
+            perror(NULL);
+            close(sockfd);
+            exit(1);
+        }
+        pthread_create(&pt, NULL, stream, &newsockfd);
+        pthread_detach(pt);
+    }
 }
